6d8b09781c2d1681e8857b8416efe6112b7ba54c
Muhammed Ayman <mou.rou.free@gmail.com>
2026-05-12 12:50:54 +0300

    Convert app to PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,70 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+
+  plugins: [
+
+    react(),
+
+    VitePWA({
+
+      registerType: "autoUpdate",
+
+      
+      workbox: {
+
+      maximumFileSizeToCacheInBytes:
+        5 * 1024 * 1024
+
+    },
+
+      manifest: {
+
+        name: "ERP System",
+
+        short_name: "ERP",
+
+        description:
+          "Management & Inventory System",
+
+        theme_color: "#111827",
+
+        background_color: "#f8fafc",
+
+        display: "standalone",
+
+        orientation: "portrait",
+
+        scope: "/",
+
+        start_url: "/",
+
+        icons: [
+
+          {
+            src: "/pwa-192.png",
+
+            sizes: "192x192",
+
+            type: "image/png"
+          },
+
+          {
+            src: "/pwa-512.png",
+
+            sizes: "512x512",
+
+            type: "image/png"
+          }
+
+        ]
+
+      }
+
+    })
+
+  ]
+
+});
